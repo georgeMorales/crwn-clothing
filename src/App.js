221c086c97c0cf300b32dd3dac7c44c883162b60
jorge@jorge-PE60-6QE <moraleslopezjorge@gmail.com>
2019-12-31 13:38:35 +0100

    updating shop page, collection preview y collection item component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 
-const HatsPage = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h1>HATS PAGE</h1>
-        </div>
-    )
-}
+
+// const HatsPage = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//             <h1>HATS PAGE</h1>
+//         </div>
+//     )
+// }
 
 // const TopicsList = () => {
 //     return (
@@ -34,7 +36,8 @@ function App() {
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/hats' component={HatsPage} />
+                {/* <Route path='/hats' component={HatsPage} /> */}
+                <Route path='/shop' component={ShopPage} />
                 
             </Switch>
         </div>
